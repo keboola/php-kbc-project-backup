bbbc37bfbc2530c437d3af87e1a22548c55ea8e5
Erik Zigo <erik.zigo@keboola.com>
2018-04-24 08:54:24 +0200

    init commit

---
+++ src/BackupS3.php
@@ -0,0 +1,241 @@
+<?php
+namespace Keboola\StorageApi\Project;
+
+use Aws\S3\S3Client;
+use Keboola\StorageApi\Client AS StorageApi;
+use Keboola\StorageApi\HandlerStack;
+use Keboola\StorageApi\Options\GetFileOptions;
+use Keboola\Temp\Temp;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Component\Filesystem\Filesystem;
+
+class BackupS3
+{
+    private const CONFIGS_VERSION_LIMIT = 2;
+
+    /**
+     * @var StorageApi
+     */
+    private $sapiClient;
+
+    /**
+     * @var S3Client
+     */
+    private $s3Client;
+
+    /**
+     * @var NullLogger
+     */
+    private $logger;
+
+    public function __construct(StorageApi $sapiClient, S3Client $s3Client, LoggerInterface $logger = null)
+    {
+        $this->sapiClient = $sapiClient;
+        $this->s3Client = $s3Client;
+        $this->logger = $logger?: new NullLogger();
+    }
+
+    public function backup(string $targetBucket, string $targetBasePath = null): void
+    {
+        $onlyStructure = false;
+        $includeVersions = true;
+        if ($targetBasePath === '' || $targetBasePath === '/') {
+            $targetBasePath = '';
+        } else {
+            $targetBasePath = rtrim($targetBasePath, '/') . '/';
+        }
+
+        //@FIXME backup methods cannot return void
+
+        $tables = $this->backupTablesMetadata($targetBucket, $targetBasePath);
+        usort($tables, function ($a, $b) {
+            return strcmp($a["id"], $b["id"]);
+        });
+
+        //@FIXME resolve counter? (backup stats?) custom line
+        foreach (array_values($tables) as $i => $table) {
+            if ($onlyStructure && $table['bucket']['stage'] !== 'sys') {
+                $this->logger->warning(sprintf('Skipping table %s (sys bucket)', $table['id']));
+            } elseif (!$table['isAlias']) {
+                $this->backupTable($table['id'], $targetBucket, $targetBasePath);
+            } else {
+                $this->logger->warning(sprintf('Skipping table %s (alias)', $table['id']));
+            }
+        }
+
+        $this->backupConfigs($targetBucket, $targetBasePath, $includeVersions);
+    }
+
+
+    public function backupTablesMetadata(string $targetBucket, string $targetBasePath): array
+    {
+        $this->logger->info('Exporting buckets');
+
+        $this->s3Client->putObject([
+            'Bucket' => $targetBucket,
+            'Key' => $targetBasePath . 'buckets.json',
+            'Body' => json_encode($this->sapiClient->listBuckets(["include" => "attributes,metadata"])),
+        ]);
+
+        $this->logger->info('Exporting tables');
+        $tables = $this->sapiClient->listTables(null, [
+            'include' => 'attributes,columns,buckets,metadata,columnMetadata',
+        ]);
+
+        $this->s3Client->putObject([
+            'Bucket' => $targetBucket,
+            'Key' => $targetBasePath . 'tables.json',
+            'Body' => json_encode($tables),
+        ]);
+
+        return $tables;
+        return array_map(function ($row) { return $row['id']; }, $tables);
+    }
+
+    public function backupTable(string $tableId, string $targetBucket, string $targetBasePath): void
+    {
+        $this->logger->info(sprintf('Exporting table %s', $tableId));
+
+        $tmp = new Temp();
+        $tmp->initRunFolder();
+
+        $fileId = $this->sapiClient->exportTableAsync($tableId, [
+            'gzip' => true,
+        ]);
+        $fileInfo = $this->sapiClient->getFile($fileId["file"]["id"], (new GetFileOptions())->setFederationToken(true));
+
+        $this->logger->info('region' . $fileInfo["region"]); //@FIXME remove
+
+        // Initialize S3Client with credentials from Storage API
+        $s3Client = new S3Client([
+            "version" => "latest",
+            "region" => $fileInfo["region"],
+            "credentials" => [
+                "key" => $fileInfo["credentials"]["AccessKeyId"],
+                "secret" => $fileInfo["credentials"]["SecretAccessKey"],
+                "token" => $fileInfo["credentials"]["SessionToken"],
+            ],
+        ]);
+
+        $fs = new Filesystem();
+        if ($fileInfo['isSliced'] === true) {
+            // Download manifest with all sliced files
+            $client = new \GuzzleHttp\Client([
+                'handler' => HandlerStack::create([
+                    'backoffMaxTries' => 10,
+                ]),
+            ]);
+            $manifest = json_decode($client->get($fileInfo['url'])->getBody(), true);
+
+            // Download all slices
+            //@FIXME better temps
+            $tmpFilePath = $tmp->getTmpFolder() . DIRECTORY_SEPARATOR . uniqid('sapi-export-');
+            foreach ($manifest["entries"] as $i => $part) {
+                $fileKey = substr($part["url"], strpos($part["url"], '/', 5) + 1);
+                $filePath = $tmpFilePath . '_' . md5(str_replace('/', '_', $fileKey));
+                $this->s3Client->getObject(array(
+                    'Bucket' => $fileInfo["s3Path"]["bucket"],
+                    'Key' => $fileKey,
+                    'SaveAs' => $filePath
+                ));
+                $fh = fopen($filePath, 'r');
+                $this->s3Client->putObject([
+                    'Bucket' => $targetBucket,
+                    'Key' => $targetBasePath . str_replace('.', '/', $tableId) . '.part_' . $i . '.csv.gz',
+                    'Body' => $fh,
+                ]);
+                fclose($fh);
+                $fs->remove($filePath);
+            }
+        } else {
+            $tmpFilePath = $tmp->getTmpFolder() . DIRECTORY_SEPARATOR . uniqid('table');
+            $s3Client->getObject(array(
+                'Bucket' => $fileInfo["s3Path"]["bucket"],
+                'Key' => $fileInfo["s3Path"]["key"],
+                'SaveAs' => $tmpFilePath
+            ));
+
+            $fh = fopen($tmpFilePath, 'r');
+            $this->s3Client->putObject([
+                'Bucket' => $targetBucket,
+                'Key' => $targetBasePath . str_replace('.', '/', $tableId) . '.csv.gz',
+                'Body' => $fh,
+            ]);
+            fclose($fh);
+            $fs->remove($tmpFilePath);
+        }
+    }
+
+
+    public function backupConfigs($targetBucket, $targetBasePath = null, $saveVersions): void
+    {
+        $limit = self::CONFIGS_VERSION_LIMIT;
+        $this->logger->info('Exporting configurations');
+
+        $tmp = new Temp();
+        $tmp->initRunFolder();
+
+        $configurationsFile = $tmp->createFile("configurations.json");
+        $versionsFile = $tmp->createFile("versions.json");
+
+        // use raw api call to prevent parsing json - preserve empty JSON objects
+        $this->sapiClient->apiGet('storage/components?include=configuration', $configurationsFile->getPathname());
+        $handle = fopen($configurationsFile, "r");
+        $this->s3Client->putObject([
+            'Bucket' => $targetBucket,
+            'Key' => $targetBasePath . 'configurations.json',
+            'Body' => $handle,
+        ]);
+        fclose($handle);
+
+        $url = "storage/components";
+        $url .= "?include=configuration,rows,state";
+        $this->sapiClient->apiGet($url, $configurationsFile->getPathname());
+        $configurations = json_decode(file_get_contents($configurationsFile->getPathname()));
+
+        foreach ($configurations as $component) {
+            $this->logger->info(sprintf('Exporting %s configurations', $component->id));
+
+            foreach ($component->configurations as $configuration) {
+                if ($saveVersions) {
+                    $offset = 0;
+                    $versions = [];
+                    do {
+                        $url = "storage/components/{$component->id}/configs/{$configuration->id}/versions";
+                        $url .= "?include=name,description,configuration,state";
+                        $url .= "&limit={$limit}&offset={$offset}";
+                        $this->sapiClient->apiGet($url, $versionsFile->getPathname());
+                        $versionsTmp = json_decode(file_get_contents($versionsFile->getPathname()));
+                        $versions = array_merge($versions, $versionsTmp);
+                        $offset = $offset + $limit;
+                    } while (count($versionsTmp) > 0);
+                    $configuration->_versions = $versions;
+                }
+                if ($saveVersions) {
+                    foreach ($configuration->rows as &$row) {
+                        $offset = 0;
+                        $versions = [];
+                        do {
+                            $url = "storage/components/{$component->id}/configs/{$configuration->id}/rows/{$row->id}/versions";
+                            $url .= "?include=configuration";
+                            $url .= "&limit={$limit}&offset={$offset}";
+                            $this->sapiClient->apiGet($url, $versionsFile->getPathname());
+                            $versionsTmp = json_decode(file_get_contents($versionsFile->getPathname()));
+                            $versions = array_merge($versions, $versionsTmp);
+                            $offset = $offset + $limit;
+                        } while (count($versionsTmp) > 0);
+                        $row->_versions = $versions;
+                    }
+                }
+                $this->s3Client->putObject([
+                    'Bucket' => $targetBucket,
+                    'Key' => $targetBasePath . 'configurations/' . $component->id . '/' .
+                        $configuration->id . '.json',
+                    'Body' => json_encode($configuration),
+                ]);
+            }
+        }
+    }
+
+}
---
+++ tests/BackupS3Tests.php
@@ -0,0 +1,451 @@
+<?php
+
+namespace Keboola\StorageApi\Cli\Tests\Command;
+
+use Aws\S3\S3Client;
+use Keboola\Csv\CsvFile;
+use Keboola\StorageApi\Client as StorageApi;
+use Keboola\StorageApi\Components;
+use Keboola\StorageApi\Metadata;
+use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ConfigurationRow;
+use Keboola\StorageApi\Project\BackupS3;
+use Keboola\Temp\Temp;
+
+class BackupProjectTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var StorageApi
+     */
+    private $sapiClient;
+
+    /**
+     * @var S3Client
+     */
+    private $s3Client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->sapiClient = new StorageApi([
+            'url' => TEST_STORAGE_API_URL,
+            'token' => TEST_STORAGE_API_TOKEN,
+        ]);
+
+        $this->cleanupKbcProject();
+
+        putenv('AWS_ACCESS_KEY_ID=' . TEST_AWS_ACCESS_KEY_ID);
+        putenv('AWS_SECRET_ACCESS_KEY=' . TEST_AWS_SECRET_ACCESS_KEY);
+
+        $this->sapiClient = new S3Client([
+            'version' => 'latest',
+            'region' => TEST_AWS_REGION,
+        ]);
+
+        $this->cleanupS3();
+    }
+
+    public function testExecuteNoVersions(): void
+    {
+        $component = new Components($this->sapiClient);
+
+        $config = new Configuration();
+        $config->setComponentId('transformation');
+        $config->setDescription('Test Configuration');
+        $config->setConfigurationId('sapi-php-test');
+        $config->setName('test-configuration');
+        $configData = $component->addConfiguration($config);
+        $config->setConfigurationId($configData['id']);
+
+        $row = new ConfigurationRow($config);
+        $row->setChangeDescription('Row 1');
+        $row->setConfiguration(
+            ['name' => 'test 1', 'backend' => 'docker', 'type' => 'r', 'queries' => ['foo']]
+        );
+        $component->addConfigurationRow($row);
+
+        $row = new ConfigurationRow($config);
+        $row->setChangeDescription('Row 2');
+        $row->setConfiguration(
+            ['name' => 'test 2', 'backend' => 'docker', 'type' => 'r', 'queries' => ['bar']]
+        );
+        $component->addConfigurationRow($row);
+
+        $backup = new BackupS3($this->sapiClient, $this->s3Client);
+        $backup->backup(TEST_S3_BUCKET, 'backup');
+
+        $temp = new Temp();
+        $temp->initRunFolder();
+
+        $targetFile = $temp->createTmpFile('configurations.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/configurations.json',
+            'SaveAs' => $targetFile,
+        ]);
+
+        $targetContents = file_get_contents($targetFile);
+        $targetData = json_decode($targetContents, true);
+        $targetComponent = [];
+        foreach ($targetData as $component) {
+            if ($component['id'] == 'transformation') {
+                $targetComponent = $component;
+                break;
+            }
+        }
+        self::assertGreaterThan(0, count($targetComponent));
+
+        $targetConfiguration = [];
+        foreach ($targetComponent['configurations'] as $configuration) {
+            if ($configuration['name'] == 'test-configuration') {
+                $targetConfiguration = $configuration;
+            }
+        }
+        self::assertGreaterThan(0, count($targetConfiguration));
+        self::assertEquals('Test Configuration', $targetConfiguration['description']);
+        self::assertNotContains('rows', $targetConfiguration);
+
+        $configurationId = $targetConfiguration['id'];
+        $targetFile = $temp->createTmpFile('configurations.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/configurations/transformation/' . $configurationId . '.json',
+            'SaveAs' => $targetFile,
+        ]);
+        $targetContents = file_get_contents($targetFile);
+        $targetConfiguration = json_decode($targetContents, true);
+        self::assertGreaterThan(0, count($targetConfiguration));
+        self::assertEquals('test-configuration', $targetConfiguration['name']);
+        self::assertEquals('Test Configuration', $targetConfiguration['description']);
+        self::assertArrayHasKey('rows', $targetConfiguration);
+        self::assertEquals(2, count($targetConfiguration['rows']));
+        self::assertEquals('foo', $targetConfiguration['rows'][0]['configuration']['queries'][0]);
+        self::assertEquals('bar', $targetConfiguration['rows'][1]['configuration']['queries'][0]);
+        self::assertNotContains('versions', $targetConfiguration);
+        self::assertNotContains('versions', $targetConfiguration['rows'][0]);
+    }
+
+    /**
+     * @dataProvider largeConfigurationsProvider
+     * @param int $configurationRowsCount
+     * @throws \Exception
+     */
+    public function testLargeConfigurations(int $configurationRowsCount): void
+    {
+        $component = new Components($this->sapiClient);
+
+        $config = new Configuration();
+        $config->setComponentId('transformation');
+        $config->setDescription('Test Configuration');
+        $config->setConfigurationId('sapi-php-test');
+        $config->setName('test-configuration');
+        $config->setState(['key' => 'value']);
+        $configData = $component->addConfiguration($config);
+        $config->setConfigurationId($configData['id']);
+
+        $largeRowConfiguration = [
+            'values' => [],
+        ];
+        $valuesCount = 100;
+        for ($i = 0; $i < $valuesCount; $i++) {
+            $largeRowConfiguration['values'][] = sha1(random_bytes(128));
+        }
+
+        for ($i = 0; $i < $configurationRowsCount; $i++) {
+            $row = new ConfigurationRow($config);
+            $row->setChangeDescription('Row 1');
+            $row->setConfiguration($largeRowConfiguration);
+
+            if ($i === 0) {
+                $row->setState(['rowKey' => 'value']);
+            }
+
+            $component->addConfigurationRow($row);
+        }
+
+        $backup = new BackupS3($this->sapiClient, $this->s3Client);
+        $backup->backupConfigs(TEST_S3_BUCKET, 'backup', false);
+
+        $temp = new Temp();
+        $temp->initRunFolder();
+
+        $targetFile = $temp->createTmpFile($config->getConfigurationId() . 'configurations.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/configurations/transformation/' . $config->getConfigurationId() . '.json',
+            'SaveAs' => $targetFile,
+        ]);
+        $targetContents = file_get_contents($targetFile);
+        $targetConfiguration = json_decode($targetContents, true);
+        self::assertGreaterThan(0, count($targetConfiguration));
+        self::assertEquals('test-configuration', $targetConfiguration['name']);
+        self::assertEquals('Test Configuration', $targetConfiguration['description']);
+        self::assertEquals(['key' => 'value'], $targetConfiguration['state']);
+        self::assertArrayHasKey('rows', $targetConfiguration);
+        self::assertCount($configurationRowsCount, $targetConfiguration['rows']);
+
+        $firstRow = reset($targetConfiguration['rows']);
+        $this->assertEquals(['rowKey' => 'value'], $firstRow['state']);
+        $lastRow = end($targetConfiguration['rows']);
+        $this->assertEmpty($lastRow['state']);
+    }
+
+    public function largeConfigurationsProvider(): array
+    {
+        return [
+            [
+                10,
+            ],
+            [
+                30,
+            ],
+        ];
+    }
+
+    public function testPreserveEmptyObjectAndArray(): void
+    {
+        $component = new Components($this->sapiClient);
+
+        $config = new Configuration();
+        $config->setComponentId('transformation');
+        $config->setDescription('Test Configuration');
+        $config->setConfigurationId('sapi-php-test');
+        $config->setName('test-configuration');
+        $config->setConfiguration(
+            [
+                "dummyObject" => new \stdClass(),
+                "dummyArray" => [],
+            ]
+        );
+        $configData = $component->addConfiguration($config);
+        $config->setConfigurationId($configData['id']);
+
+        $row = new ConfigurationRow($config);
+        $row->setChangeDescription('Row 1');
+        $row->setConfiguration(
+            [
+                'name' => 'test 1',
+                'backend' => 'docker',
+                'type' => 'r',
+                'queries' => ['foo'],
+                "dummyObject" => new \stdClass(),
+                "dummyArray" => [],
+            ]
+        );
+        $component->addConfigurationRow($row);
+
+        $row = new ConfigurationRow($config);
+        $row->setChangeDescription('Row 2');
+        $row->setConfiguration(
+            [
+                'name' => 'test 2',
+                'backend' => 'docker',
+                'type' => 'r',
+                'queries' => ['bar'],
+                "dummyObject" => new \stdClass(),
+                "dummyArray" => [],
+            ]
+        );
+        $component->addConfigurationRow($row);
+
+        $backup = new BackupS3($this->sapiClient, $this->s3Client);
+        $backup->backupConfigs(TEST_S3_BUCKET, 'backup', false); //@FIXME maybe tests versions too
+
+        $temp = new Temp();
+        $temp->initRunFolder();
+
+        $targetFile = $temp->createTmpFile('configurations.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/configurations.json',
+            'SaveAs' => $targetFile,
+        ]);
+        $targetContents = file_get_contents($targetFile);
+        $targetData = json_decode($targetContents);
+        $targetConfiguration = $targetData[0]->configurations[0];
+
+        self::assertEquals(new \stdClass(), $targetConfiguration->configuration->dummyObject);
+        self::assertEquals([], $targetConfiguration->configuration->dummyArray);
+
+        $configurationId = $targetConfiguration->id;
+        $targetFile = $temp->createTmpFile($configurationId . 'configurations.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/configurations/transformation/' . $configurationId . '.json',
+            'SaveAs' => $targetFile,
+        ]);
+        $targetContents = file_get_contents($targetFile);
+        $targetConfiguration = json_decode($targetContents);
+
+        self::assertEquals(new \stdClass(), $targetConfiguration->rows[0]->configuration->dummyObject);
+        self::assertEquals([], $targetConfiguration->rows[0]->configuration->dummyArray);
+    }
+
+    public function testExecuteLinkedBuckets(): void
+    {
+        $bucketId = $this->sapiClient->createBucket("main", StorageApi::STAGE_IN);
+
+        $this->sapiClient->setBucketAttribute($bucketId, 'key', 'value', true);
+        $this->sapiClient->shareBucket($bucketId, ['sharing' => 'organization']);
+
+        $this->sapiClient->createTable("in.c-main", "sample", new CsvFile(__DIR__ . "/../data/sample.csv"));
+
+        $token = $this->sapiClient->verifyToken();
+        $projectId = $token['owner']['id'];
+
+        $this->sapiClient->linkBucket('linked', 'in', $projectId, $bucketId);
+
+        $backup = new BackupS3($this->sapiClient, $this->s3Client);
+        $backup->backupTablesMetadata(TEST_S3_BUCKET, 'backup');
+
+        $temp = new Temp();
+        $temp->initRunFolder();
+
+        $targetFile = $temp->createTmpFile('buckets.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/buckets.json',
+            'SaveAs' => $targetFile,
+        ]);
+        $buckets = json_decode(file_get_contents($targetFile), true);
+
+        self::assertCount(2, $buckets);
+        self::assertNotEmpty($buckets[1]['sourceBucket']);
+
+        $targetFile = $temp->createTmpFile('tables.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/tables.json',
+            'SaveAs' => $targetFile,
+        ]);
+
+        $tables = json_decode(file_get_contents($targetFile), true);
+
+        self::assertCount(2, $tables);
+        self::assertNotEmpty($tables[1]['sourceTable']);
+    }
+
+    public function testExecuteMetadata(): void
+    {
+        $this->sapiClient->createBucket("main", StorageApi::STAGE_IN);
+        $this->sapiClient->createTable("in.c-main", "sample", new CsvFile(__DIR__ . "/../data/sample.csv"));
+
+        $metadata = new Metadata($this->sapiClient);
+        $metadata->postBucketMetadata("in.c-main", "system", [
+            [
+                "key" => "bucketKey",
+                "value" => "bucketValue",
+            ],
+        ]);
+        $metadata->postTableMetadata("in.c-main.sample", "system", [
+            [
+                "key" => "tableKey",
+                "value" => "tableValue",
+            ],
+        ]);
+        $metadata->postColumnMetadata("in.c-main.sample.col1", "system", [
+            [
+                "key" => "columnKey",
+                "value" => "columnValue",
+            ],
+        ]);
+
+        $backup = new BackupS3($this->sapiClient, $this->s3Client);
+        $backup->backupTablesMetadata(TEST_S3_BUCKET, 'backup');
+
+        $temp = new Temp();
+        $temp->initRunFolder();
+
+        $targetFile = $temp->createTmpFile('buckets.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/buckets.json',
+            'SaveAs' => $targetFile,
+        ]);
+
+        $data = json_decode(file_get_contents($targetFile), true);
+        $this->assertEquals("bucketKey", $data[0]["metadata"][0]["key"]);
+        $this->assertEquals("bucketValue", $data[0]["metadata"][0]["value"]);
+
+        $targetFile = $temp->createTmpFile('tables.json');
+        $this->s3Client->getObject([
+            'Bucket' => TEST_S3_BUCKET,
+            'Key' => 'backup/tables.json',
+            'SaveAs' => $targetFile,
+        ]);
+        $data = json_decode(file_get_contents($targetFile), true);
+        $this->assertEquals("tableKey", $data[0]["metadata"][0]["key"]);
+        $this->assertEquals("tableValue", $data[0]["metadata"][0]["value"]);
+        $this->assertEquals("columnKey", $data[0]["columnMetadata"]["col1"][0]["key"]);
+        $this->assertEquals("columnValue", $data[0]["columnMetadata"]["col1"][0]["value"]);
+    }
+
+    public function testExecuteWithoutPath(): void
+    {
+        $this->sapiClient->createBucket("main", StorageApi::STAGE_IN);
+        $this->sapiClient->createTable("in.c-main", "sample", new CsvFile(__DIR__ . "/../data/sample.csv"));
+
+        $backup = new BackupS3($this->sapiClient, $this->s3Client);
+        $backup->backupTablesMetadata(TEST_S3_BUCKET, '');
+        $backup->backupConfigs(TEST_S3_BUCKET, '');
+
+        $keys = array_map(function ($key) {
+            return $key["Key"];
+        }, $this->s3Client->listObjects([
+            'Bucket' => TEST_S3_BUCKET,
+        ])->toArray()["Contents"]);
+
+        self::assertTrue(in_array('buckets.json', $keys));
+        self::assertTrue(in_array('tables.json', $keys));
+        self::assertTrue(in_array('configurations.json', $keys));
+        self::assertCount(3, $keys);
+    }
+
+    public function tearDown(): void
+    {
+        $this->cleanupKbcProject();
+        $this->cleanupS3();
+    }
+
+    private function cleanupKbcProject()
+    {
+        $components = new Components($this->sapiClient);
+        foreach ($components->listComponents() as $component) {
+            foreach ($component['configurations'] as $configuration) {
+                $components->deleteConfiguration($component['id'], $configuration['id']);
+            }
+        }
+
+        // drop linked buckets
+        foreach ($this->sapiClient->listBuckets() as $bucket) {
+            if (isset($bucket['sourceBucket'])) {
+                $this->sapiClient->dropBucket($bucket["id"], ["force" => true]);
+            }
+        }
+
+        foreach ($this->sapiClient->listBuckets() as $bucket) {
+            $this->sapiClient->dropBucket($bucket["id"], ["force" => true]);
+        }
+    }
+
+    private function cleanupS3()
+    {
+        $keys = $this->s3Client->listObjects(['Bucket' => TEST_S3_BUCKET]);
+        $keys = $keys->toArray()['Contents'];
+        $deleteObjects = [];
+        foreach ($keys as $key) {
+            $deleteObjects[] = $key;
+        }
+
+        if (count($deleteObjects) > 0) {
+            $this->s3Client->deleteObjects(
+                [
+                    'Bucket' => TEST_S3_BUCKET,
+                    'Delete' => ['Objects' => $deleteObjects],
+                ]
+            );
+        }
+
+    }
+}
